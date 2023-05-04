8c8fdfe876e0adb3f3a49fd23acc80d8376174cd
Aminembarek2 <62722834+Aminembarek2@users.noreply.github.com>
2023-05-04 18:42:10 +0200

    update sql url

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 import urllib.parse 
 
 # Configure your database
+params = urllib.parse.quote_plus(os.environ["AWSSQLConnectionString"])
 
 class Config:
-    
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@localhost/autonews'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s'% params
     SQLALCHEMY_TRACK_MODIFICATIONS = False
